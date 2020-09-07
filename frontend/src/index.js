84bdb4bbc4cc44b958a26e33b4a3b4c1f95406d1
Psouza-10 <paulosouza300272@gmail.com>
2020-09-07 01:31:45 -0300

    heroku attempt #7887684

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,7 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 import { GlobalProvider } from './Context'
-import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:5000'
 ReactDOM.render(
   <React.StrictMode>
     <GlobalProvider>
