c2d40bb52334ef2fa0d6beb3f2546fc193122865
Psouza-10 <paulosouza300272@gmail.com>
2020-09-06 23:38:06 -0300

    deploy configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
 const app = require('./app')
+const express = require('express')
+const path = require('path')
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const cloudinary = require('cloudinary').v2
@@ -10,4 +12,15 @@ cloudinary.config({
   api_key: API_KEY,
   api_secret: API_SECRET
 })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../../frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    )
+  })
+}
+
 app.listen(PORT, () => console.log(`Servidor inicializado na Porta ${PORT}`))
