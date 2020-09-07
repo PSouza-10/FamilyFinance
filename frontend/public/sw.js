0642bb29ac3e2b6589353a1f2067521a91a58ce9
Psouza-10 <paulosouza300272@gmail.com>
2020-09-07 14:19:17 -0300

    PWA testing

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,16 @@
 const CACHE_NAME = 'v-1'
 
-const cacheUrls = ['index.html', 'offline.html']
+const cacheUrls = [
+  'index.html',
+  'offline.html',
+  '/',
+  '/static/js/bundle.js',
+  '/static/js/main.chunk.js',
+  '/static/js/1.chunk.js',
+  '/static/js/0.chunk.js',
+  '/favicon.ico',
+  'https://fonts.googleapis.com/css2?family=Roboto&display=swap'
+]
 
 const self = this
 
@@ -8,15 +18,21 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log('Opened Cache')
-      return cache.addAll(cacheUrls)
+      return cache.addAll(
+        cacheUrls.map(url => {
+          return new Request(url, { mode: 'no-cors' })
+        })
+      )
     })
   )
 })
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(() => {
-      return fetch(e.request).catch(() => caches.match('offline.html'))
+    caches.match(e.request).then(response => {
+      return (
+        response || fetch(e.request).catch(() => caches.match('offline.html'))
+      )
     })
   )
 })
